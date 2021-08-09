2a7bea9386de553db34f2d1365d44a49d0b0be4d
tanverified <khantanveer2u@gmail.com>
2021-08-09 19:34:54 +0530

    validation

--- express-demo/courses.js
+++ express-demo/courses.js
@@ -1,3 +1,4 @@
+const Joi = require("Joi");
 const express = require("express");
 const app = express();
 
@@ -18,6 +19,25 @@ app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+});
+
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) {
@@ -25,14 +45,6 @@ app.get("/api/courses/:id", (req, res) => {
   } else {
     res.send(course);
   }
-});
-
-app.post("/api/courses", (req, res) => {
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
 });
 
 const port = process.env.PORT || 3000;
